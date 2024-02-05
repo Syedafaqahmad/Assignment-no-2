72be772cad3f742ad2c98009d57d05fd16ef9b19
Syedafaqahmad <82577242+Syedafaqahmad@users.noreply.github.com>
2024-02-05 08:01:46 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+
+
+// Question=1
+
+// var a = 10 
+// alert ("The value of a is:" + a)
+
+// alert("The value of ++a" + ++a) 
+
+// alert ("The value of a is:" + a)
+
+// alert ("The value of a++ is:" + a++)
+
+// alert ("The value of a is:" + a)
+
+// alert("The value of --a" + --a) 
+
+// alert ("The value of a is:" + a)
+
+// alert ("The value of a-- is:" + a--)
+
+// alert ("The value of a is:" + a)
+
+
+// question 2 
+
+// var a = 2
+// var b = 1 
+// alert("The value of a is " +a )
+// alert("The value of --a is " + --a)
+// alert("The value of b is " + b) 
+// (var result = --a - --b
+// alert("The value of  --a - --b is " + result  )
+
+// alert("The value of a is " +a )
+// alert("The value of b is " + b)
+// result = --a - --b + ++b
+// alert("The vaoue of --a - --b + ++b is " + result)
+// alert("The value of a is " +a )
+// alert("The value of b is " + b)
+
+// result = --a - --b + ++b + b--
+
+// alert("The vaoue of --a - --b + ++b + b-- is " + result)
+// alert("The value of a is " +a )
+// alert("The value of b is " + b)
+
+
+// question3 
+// var userName = prompt("Enter user Name") 
+
+// alert("Welcome " + userName)
+
+// question4 
+
+let tableNo = +prompt('Enter Table No here')
+
+if(tableNo == 0){
+  tableNo = 5
+}
+
+for(i = 1; i <=10; i++){
+  console.log(tableNo + " X " + i + " = " + tableNo * i)
+}
+
+var sub1 = prompt("Enter ")
